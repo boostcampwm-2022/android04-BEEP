bdc56ff8fb33584557956f2543bb746eeea48abd
mangbaam <pmb0836@gmail.com>
2022-11-07 16:31:41 +0900

    init: 멀티 모듈 구조로 초기화

---
+++ presentation/src/main/java/com/lighthouse/presentation/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.lighthouse.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
