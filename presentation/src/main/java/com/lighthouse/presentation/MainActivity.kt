b4fe66b479018ff95f536a95409bed2a53cdcc46
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-10 10:31:57 +0900

    Issues #1 test: github issues 연동 테스트

--- presentation/src/main/java/com/lighthouse/presentation/MainActivity.kt
+++ presentation/src/main/java/com/lighthouse/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // 테스트
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
