3d5a8fcd30a58093e7fb9732fd5acc3727f5f021
김명석 <audxo112@naver.com>
2022-11-10 23:48:36 +0900

    Issues #16 feat: MainActivity 이동 구현
    - 기프티콘 목록, 홈, 설정 프레그먼트로 이동
    - 기프티콘 추가 액티비티로 이동

--- presentation/src/main/java/com/lighthouse/presentation/ui/gifticonlist/GifticonListFragment.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/gifticonlist/GifticonListFragment.kt
@@ -2,10 +2,10 @@ package com.lighthouse.presentation.ui.gifticonlist
 
 import androidx.fragment.app.Fragment
 import com.lighthouse.presentation.R
-import com.lighthouse.presentation.databinding.FragmentListBinding
+import com.lighthouse.presentation.databinding.FragmentGifticonListBinding
 import com.lighthouse.presentation.ui.common.viewBindings
 
 class GifticonListFragment : Fragment(R.layout.fragment_gifticon_list) {
 
-    private val binding by viewBindings(FragmentListBinding::bind)
+    private val binding by viewBindings(FragmentGifticonListBinding::bind)
 }
