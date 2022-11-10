b0d70df52e3648c76b538dda7fcf83c1ca7217a5
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-10 23:19:35 +0900

    Issues #13 refactor: viewModel provider -> by viewModels

--- presentation/src/main/java/com/lighthouse/presentation/ui/map/MapActivity.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/map/MapActivity.kt
@@ -1,20 +1,19 @@
 package com.lighthouse.presentation.ui.map
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.lighthouse.presentation.R
 import com.lighthouse.presentation.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMapBinding
-    private lateinit var viewModel: MapViewModel // TODO by viewModels + Hilt 로 리팩토링 예정
+    private val viewModel: MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
-        viewModel = ViewModelProvider(this)[MapViewModel::class.java]
     }
 }
