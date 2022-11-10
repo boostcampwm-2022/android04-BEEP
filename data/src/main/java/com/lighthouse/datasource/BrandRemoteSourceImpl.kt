07855bcd1da0f87dc29d26fa3b82522b0ffafb21
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-11 02:54:23 +0900

    Issues #24 feat: 카카오 서치 로직틀 구현
    - 보내는 로직 viewModel(키워드 등등) -> UseCase -> Repository -> DataSource -> NetworkApiService
    - 응답 로직 NetworkApiService -> DataSource -> Repository -> UseCase -> viewModel
    - Binds와 Provide를 활용해서 Di 주입

---
+++ data/src/main/java/com/lighthouse/datasource/BrandRemoteSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.lighthouse.datasource
+
+import com.lighthouse.model.BrandPlaceInfoDataContainer
+import com.lighthouse.model.CustomErrorData
+import com.lighthouse.network.NetworkApiService
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+class BrandRemoteSourceImpl @Inject constructor(
+    private val networkApiService: NetworkApiService
+) : BrandRemoteSource {
+
+    override suspend fun getBrandPlaceInfo(
+        brandName: String,
+        x: String,
+        y: String,
+        rect: String,
+        size: Int
+    ): Result<BrandPlaceInfoDataContainer> {
+        val result = runCatching { networkApiService.getAllBrandPlaceInfo(brandName, x, y, rect, size) }
+
+        return when (val exception = result.exceptionOrNull()) {
+            null -> result
+            is UnknownHostException -> Result.failure(CustomErrorData.NetworkFailure)
+            else -> Result.failure(exception)
+        }
+    }
+}
---
+++ data/src/main/java/com/lighthouse/repository/BrandRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.lighthouse.repository
+
+import com.lighthouse.datasource.BrandRemoteSource
+import com.lighthouse.domain.model.BrandPlaceInfo
+import com.lighthouse.domain.repository.BrandRepository
+import com.lighthouse.mapper.toDomain
+import com.lighthouse.model.CustomErrorData
+import javax.inject.Inject
+
+class BrandRepositoryImpl @Inject constructor(
+    private val remoteBrandSource: BrandRemoteSource
+) : BrandRepository {
+
+    override suspend fun getBrandPlaceInfo(
+        brandName: String,
+        x: String,
+        y: String,
+        rect: String,
+        size: Int
+    ): Result<List<BrandPlaceInfo>> {
+        val result = remoteBrandSource.getBrandPlaceInfo(brandName, x, y, rect, size).mapCatching { it.toDomain() }
+        val exception = result.exceptionOrNull()
+
+        return if (exception is CustomErrorData) {
+            Result.failure(exception.toDomain())
+        } else {
+            result
+        }
+    }
+}
