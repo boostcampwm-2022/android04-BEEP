1cbb524693e9ba00c217da08924f4f317dd57cf0
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-11-12 16:16:45 +0900

    Issues #17 style: FrameLayout -> FragmentContainerView

--- presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInActivity.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.lighthouse.presentation.R
 import com.lighthouse.presentation.databinding.ActivitySignInBinding
 
@@ -18,7 +17,7 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
         supportFragmentManager.commit {
-            add(R.id.fl_signin, signInFragment)
+            add(R.id.fcv_signin, signInFragment)
         }
     }
 }
