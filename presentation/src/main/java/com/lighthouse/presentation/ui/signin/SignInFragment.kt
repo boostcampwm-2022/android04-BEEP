38b3914873fc09615130bc745faa2403838caf38
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-11-12 17:49:25 +0900

    Issues #18 style: SignInFragment 레이아웃 구성

--- presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInFragment.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInFragment.kt
@@ -1,14 +1,17 @@
 package com.lighthouse.presentation.ui.signin
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
@@ -24,19 +27,9 @@ class SignInFragment : Fragment() {
     private val binding by viewBindings(FragmentSignInBinding::bind)
     private val viewModel: SignInViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
-    }
-
-    private fun initGoogleLogin() {
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .build()
-
-        val googleSignInClient = GoogleSignIn.getClient(requireContext(), googleSignInOptions)
-
-        val activityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val activityLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
 
@@ -52,7 +45,25 @@ class SignInFragment : Fragment() {
             }
         }
 
-        activityLauncher.launch(googleSignInClient.signInIntent)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initGoogleLogin()
+    }
+
+    private fun initGoogleLogin() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), googleSignInOptions)
+
+        binding.btnGoogleLogin.setOnClickListener {
+            activityLauncher.launch(googleSignInClient.signInIntent)
+        }
     }
 
     private fun signInWithGoogle(idToken: String) {
