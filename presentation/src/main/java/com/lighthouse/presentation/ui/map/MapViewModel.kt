09e9f31043507a3c6a9a9179f3d0395f8361d406
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-13 22:32:34 +0900

    Issues #33 remove: 필요 없는 로그, import 삭제

--- presentation/src/main/java/com/lighthouse/presentation/ui/map/MapViewModel.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/map/MapViewModel.kt
@@ -1,6 +1,5 @@
 package com.lighthouse.presentation.ui.map
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lighthouse.domain.model.CustomError
@@ -8,8 +7,6 @@ import com.lighthouse.domain.usecase.GetBrandPlaceInfosUseCase
 import com.lighthouse.presentation.mapper.toPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -32,11 +29,9 @@ class MapViewModel @Inject constructor(
             getBrandPlaceInfosUseCase(brandList, "37.2840", "127.1071", "500", 5)
                 .mapCatching { it.toPresentation() }
                 .onSuccess { brandPlaceInfos ->
-                    Log.d("TAG", "success -> $brandPlaceInfos")
                     state.value = MapState.Success(brandPlaceInfos)
                 }
                 .onFailure { throwable ->
-                    Log.d("TAG", "throwable -> $throwable")
                     when (throwable) {
                         CustomError.NetworkFailure -> state.value = MapState.NetworkFailure
                         CustomError.NotFoundBrandPlaceInfos -> state.value = MapState.NotFoundSearchResults
