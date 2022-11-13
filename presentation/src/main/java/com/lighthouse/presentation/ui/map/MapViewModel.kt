3aa83603a66eff4aa8380b0efbd6e9a5de8645a4
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-13 19:23:47 +0900

    Issues #33 feat: KakaoSearch 결과에 따른 에러 State 할당

--- presentation/src/main/java/com/lighthouse/presentation/ui/map/MapViewModel.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/map/MapViewModel.kt
@@ -1,11 +1,14 @@
 package com.lighthouse.presentation.ui.map
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.lighthouse.domain.model.CustomError
 import com.lighthouse.domain.usecase.GetBrandPlaceInfosUseCase
 import com.lighthouse.presentation.mapper.toPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,15 +19,23 @@ class MapViewModel @Inject constructor(
 
     private val brandList = arrayListOf("스타벅스", "베스킨라빈스", "BHC", "BBQ")
 
+    private val _state: MutableStateFlow<MapState> = MutableStateFlow(MapState.Loading)
+    val state: StateFlow<MapState> = _state.asStateFlow()
+
     init {
         viewModelScope.launch {
             for (query in brandList) {
                 getBrandPlaceInfosUseCase(query, "37.2840", "127.1071", "500", 5)
                     .onSuccess { brandPlaceInfos ->
-                        val brandPlaceInfoUiModels = brandPlaceInfos.map { it.toPresentation() }
-                        Log.d("TAG", "브랜드 리스트 갖고 오기 성공 -> $brandPlaceInfoUiModels")
+                        _state.value = MapState.Success(brandPlaceInfos.map { it.toPresentation() })
                     }
-                    .onFailure { Log.d("TAG", " 브랜드 리스트 갖고 오기 실패 -> $it") }
+                    .onFailure { throwable ->
+                        when (throwable) {
+                            CustomError.NetworkFailure -> _state.value = MapState.NetworkFailure
+                            CustomError.NotFoundBrandPlaceInfos -> _state.value = MapState.NotFoundSearchResults
+                            else -> _state.value = MapState.Failure
+                        }
+                    }
             }
         }
     }
