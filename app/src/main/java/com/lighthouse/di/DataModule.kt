5938d70b146d9eb1a169a79782ba7547874d29a4
김명석 <audxo112@naver.com>
2022-11-13 21:42:16 +0900

    Issues #30 feat: DI 추가

--- app/src/main/java/com/lighthouse/di/DataModule.kt
+++ app/src/main/java/com/lighthouse/di/DataModule.kt
@@ -1,12 +1,20 @@
 package com.lighthouse.di
 
+import android.content.ContentResolver
+import android.content.Context
 import com.lighthouse.datasource.BrandRemoteSource
 import com.lighthouse.datasource.BrandRemoteSourceImpl
+import com.lighthouse.datasource.GalleryImageLocalSource
+import com.lighthouse.datasource.GalleryImageLocalSourceImpl
 import com.lighthouse.domain.repository.BrandRepository
+import com.lighthouse.domain.repository.GalleryImageRepository
 import com.lighthouse.repository.BrandRepositoryImpl
+import com.lighthouse.repository.GalleryImageRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -25,4 +33,16 @@ abstract class DataModule {
     abstract fun bindBrandRepository(
         repository: BrandRepositoryImpl
     ): BrandRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindGalleryLocalDataSource(
+        source: GalleryImageLocalSourceImpl
+    ): GalleryImageLocalSource
+
+    @Binds
+    @Singleton
+    abstract fun bindGalleryImageRepository(
+        repository: GalleryImageRepositoryImpl
+    ): GalleryImageRepository
 }
---
+++ app/src/main/java/com/lighthouse/di/DatabaseModule.kt
@@ -0,0 +1,21 @@
+package com.lighthouse.di
+
+import android.content.ContentResolver
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideContentResolver(
+        @ApplicationContext context: Context
+    ): ContentResolver = context.contentResolver
+}
