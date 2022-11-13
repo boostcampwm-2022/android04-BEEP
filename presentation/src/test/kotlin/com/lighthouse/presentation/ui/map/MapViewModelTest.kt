70c74c3c22419b9e6b259c214632a2cbda2b4af7
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-13 22:06:38 +0900

    Issues #33 test: MapViewModel 테스트 코드 추가

---
+++ presentation/src/test/kotlin/com/lighthouse/presentation/ui/map/MapViewModelTest.kt
@@ -0,0 +1,92 @@
+package com.lighthouse.presentation.ui.map
+
+import com.google.common.truth.Truth
+import com.lighthouse.domain.model.BrandPlaceInfo
+import com.lighthouse.domain.model.CustomError
+import com.lighthouse.domain.usecase.GetBrandPlaceInfosUseCase
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+
+@ExperimentalCoroutinesApi
+class MapViewModelTest {
+
+    private val getBrandPlaceInfosUseCase: GetBrandPlaceInfosUseCase = mockk(relaxed = true)
+
+    private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    @DisplayName("[성공] 검색 결과를 갖고오는데 성공하면 MapState는 Success가 된다")
+    fun getBrandPlaceInfoSuccess() {
+        // given
+        coEvery {
+            getBrandPlaceInfosUseCase(brandKeyword, "37.2840", "127.1071", "500", 5)
+        } returns Result.success(brandPlaceInfo)
+
+        // when
+        val viewModel = MapViewModel(getBrandPlaceInfosUseCase)
+        viewModel.getBrandPlaceInfos(brandKeyword)
+        val actual = viewModel.state.value
+
+        // then
+        Truth.assertThat(actual).isInstanceOf(MapState.Success::class.java)
+    }
+
+    @Test
+    @DisplayName("[실패] 검색 결과가 존재하지 않는다면 MapState는 NotFoundSearchResults가 된다")
+    fun getBrandPlaceInfoNotFoundSearchResults() {
+        // given
+        coEvery {
+            getBrandPlaceInfosUseCase(brandKeyword, "37.2840", "127.1071", "500", 5)
+        } returns Result.failure(CustomError.NotFoundBrandPlaceInfos)
+
+        // when
+        val viewModel = MapViewModel(getBrandPlaceInfosUseCase)
+        viewModel.getBrandPlaceInfos(brandKeyword)
+        val actual = viewModel.state.value
+
+        // then
+        Truth.assertThat(actual).isInstanceOf(MapState.NotFoundSearchResults::class.java)
+    }
+
+    @Test
+    @DisplayName("[실패] 네트워트 연결이 문제가 생긴다면 MapState는 NetworkFailure가 된다.")
+    fun getBrandPlaceInfoNetworkError() {
+        // given
+        coEvery {
+            getBrandPlaceInfosUseCase(brandKeyword, "37.2840", "127.1071", "500", 5)
+        } returns Result.failure(CustomError.NetworkFailure)
+
+        // when
+        val viewModel = MapViewModel(getBrandPlaceInfosUseCase)
+        viewModel.getBrandPlaceInfos(brandKeyword)
+        val actual = viewModel.state.value
+
+        // then
+        Truth.assertThat(actual).isInstanceOf(MapState.NetworkFailure::class.java)
+    }
+
+    companion object {
+        private val brandKeyword: List<String> = listOf("스타벅스", "베스킨라빈스", "BBQ")
+        private val brandPlaceInfo: List<BrandPlaceInfo> = listOf(BrandPlaceInfo("서울 중구", "스타벅스", "", "", "", ""))
+    }
+}
