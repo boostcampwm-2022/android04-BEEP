55316f1c950209fcb1c4e9b83516b7c8a6256193
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-11-15 00:45:26 +0900

    Issues #39 feat: 보안 액티비티에서 Fragment 이동

--- presentation/src/main/java/com/lighthouse/presentation/ui/security/SecurityActivity.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/security/SecurityActivity.kt
@@ -3,18 +3,38 @@ package com.lighthouse.presentation.ui.security
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.lighthouse.presentation.R
+import com.lighthouse.presentation.ui.security.event.SecurityDirections
+import kotlinx.coroutines.launch
 
 class SecurityActivity : AppCompatActivity() {
 
     private val fingerprintFragment by lazy { FingerprintFragment() }
+    private val pinFragment by lazy { PinFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_security)
 
+        moveScreen(SecurityDirections.FINGERPRINT)
+    }
+
+    private fun moveScreen(directions: SecurityDirections) {
+        val fragment = when (directions) {
+            SecurityDirections.FINGERPRINT -> fingerprintFragment
+            SecurityDirections.PIN -> pinFragment
+        }
         supportFragmentManager.commit {
-            add(R.id.fcv_security, fingerprintFragment)
+            if (fragment != fingerprintFragment && fingerprintFragment.isAdded) hide(fingerprintFragment)
+            if (fragment != pinFragment && pinFragment.isAdded) hide(pinFragment)
+            if (fragment.isAdded) {
+                show(fragment)
+            } else {
+                add(R.id.fcv_security, fragment)
+            }
         }
     }
 }
