c94ef3e772f1507ff0defdc4d84a6cd53ac873e6
mangbaam <pmb0836@gmail.com>
2022-11-16 03:07:39 +0900

    Issues #44 feat: Int 를 통화 포맷으로 변경하는 확장 함수 작성

--- presentation/src/main/java/com/lighthouse/presentation/extension/KotlinExtention.kt
+++ presentation/src/main/java/com/lighthouse/presentation/extension/KotlinExtention.kt
@@ -1,12 +1,21 @@
 package com.lighthouse.presentation.extension
 
+import android.content.Context
 import android.content.res.Resources
 import android.util.TypedValue
+import com.lighthouse.presentation.R
+import java.text.DecimalFormat
 
 val Int.dp
     get() = Resources.getSystem().displayMetrics?.let { dm ->
         TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), dm)
     } ?: 0f
+
+fun Int.toConcurrency(context: Context): String {
+    val format = context.resources.getString(R.string.all_concurrency_format)
+    val formattedNumber = DecimalFormat(format).format(this)
+    return context.resources.getString(R.string.all_cash_unit, formattedNumber)
+}
 
 val screenWidth: Int
     get() = Resources.getSystem().displayMetrics?.widthPixels ?: 0
