69d667d3041d5a1e23aff111d3e833d1f42a9888
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-11-16 19:06:53 +0900

    Issues #39 fix: BiometricPrompt 를 fragment가 아닌 activity에서 띄울 때 버그 임시 해결

--- presentation/src/main/java/com/lighthouse/presentation/ui/security/fingerprint/BiometricAuth.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/security/fingerprint/BiometricAuth.kt
@@ -3,6 +3,8 @@ package com.lighthouse.presentation.ui.security.fingerprint
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.provider.Settings
 import androidx.activity.result.ActivityResultLauncher
 import androidx.annotation.RequiresApi
@@ -72,7 +74,11 @@ class BiometricAuth(
     override fun authenticate() {
         val biometricManager = BiometricManager.from(context)
         when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
-            BiometricManager.BIOMETRIC_SUCCESS -> biometricPrompt.authenticate(promptInfo)
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Handler(Looper.getMainLooper()).post {
+                    biometricPrompt.authenticate(promptInfo)
+                }
+            }
             BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> fingerprintAuthCallback.onMessagePublished((R.string.fingerprint_error_no_hardware))
             else -> fingerprintAuthCallback.onMessagePublished((R.string.fingerprint_unavailable))
         }
