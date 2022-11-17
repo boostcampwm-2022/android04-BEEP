516ce1dc242f0e683b5e0c3397b7288ccfcfd4c0
mangbaam <pmb0836@gmail.com>
2022-11-17 17:35:39 +0900

    Issues #58 feat: MainActivity 에 Hilt 어노테이션 추가 및 임시 UseCase 로부터 상세화면 표시

--- domain/src/main/java/com/lighthouse/domain/usecase/GetGifticonUseCase.kt
+++ domain/src/main/java/com/lighthouse/domain/usecase/GetGifticonUseCase.kt
@@ -1,11 +1,27 @@
 package com.lighthouse.domain.usecase
 
 import com.lighthouse.domain.model.Gifticon
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import java.util.Date
+import javax.inject.Inject
 
-class GetGifticonUseCase {
+class GetGifticonUseCase @Inject constructor() {
 
-    operator fun invoke(id: String): Gifticon {
-        return Gifticon("", "", "", "", Date(), "", true, 0, "", false)
+    // TODO (테스트 용 FAKE 제거)
+    operator fun invoke(id: String): Flow<Gifticon> {
+        return flowOf(
+            Gifticon(
+                id,
+                "",
+                "딸기마카롱설빙",
+                "설빙", Date(),
+                "998935552189",
+                true,
+                50000,
+                "삡 그리고 다운 요 삡 그리고 다운",
+                false
+            )
+        )
     }
 }
