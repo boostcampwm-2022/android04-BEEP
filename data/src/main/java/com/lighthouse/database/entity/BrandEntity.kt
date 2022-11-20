df0077fc59d7766df5a6eb158f59d378766c27bd
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-20 17:27:43 +0900

    Issues #66 feat: foreignKeys 설정

--- data/src/main/java/com/lighthouse/database/entity/BrandEntity.kt
+++ data/src/main/java/com/lighthouse/database/entity/BrandEntity.kt
@@ -2,10 +2,21 @@ package com.lighthouse.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.lighthouse.database.entity.BrandEntity.Companion.BRAND_TABLE
 
-@Entity(tableName = BRAND_TABLE)
+@Entity(
+    tableName = BRAND_TABLE,
+    foreignKeys = [
+        ForeignKey(
+            entity = SectionEntity::class,
+            parentColumns = arrayOf("section_id"),
+            childColumns = arrayOf("parent_section_id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class BrandEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
