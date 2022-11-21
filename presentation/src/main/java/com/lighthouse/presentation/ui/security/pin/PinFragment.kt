22a68c4b18a313a88ee3d0198e77b12acdadedb3
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-11-22 02:21:33 +0900

    Issues #84 feat: 보안 옵션 설정 안 함 이동 구현

--- presentation/src/main/java/com/lighthouse/presentation/ui/security/pin/PinFragment.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/security/pin/PinFragment.kt
@@ -1,5 +1,6 @@
 package com.lighthouse.presentation.ui.security.pin
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import com.lighthouse.presentation.R
 import com.lighthouse.presentation.databinding.FragmentPinBinding
 import com.lighthouse.presentation.ui.common.viewBindings
+import com.lighthouse.presentation.ui.main.MainActivity
 import com.lighthouse.presentation.ui.security.SecurityActivity
 import com.lighthouse.presentation.ui.security.event.SecurityDirections
 import kotlinx.coroutines.launch
@@ -25,12 +27,22 @@ class PinFragment : Fragment(R.layout.fragment_pin) {
         lifecycleScope.launch {
             viewModel.pinMode.collect {
                 when (it) {
-                    PinSettingType.INITIAL -> binding.tvPinDescription.text = getString(R.string.pin_initial_description)
-                    PinSettingType.CONFIRM -> binding.tvPinDescription.text = getString(R.string.pin_confirm_description)
+                    PinSettingType.INITIAL ->
+                        binding.tvPinDescription.text =
+                            getString(R.string.pin_initial_description)
+                    PinSettingType.CONFIRM ->
+                        binding.tvPinDescription.text =
+                            getString(R.string.pin_confirm_description)
                     PinSettingType.WRONG -> binding.tvPinDescription.text = getString(R.string.pin_wrong_description)
                     PinSettingType.COMPLETE -> (requireActivity() as SecurityActivity).moveScreen(SecurityDirections.FINGERPRINT)
                 }
             }
+        }
+
+        binding.tvSecureNotUse.setOnClickListener {
+            // TODO: 보안 설정 사용 X 저장
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
