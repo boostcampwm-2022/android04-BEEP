a55d2573fc563c3dacf327f89bc425bba86a117c
mangbaam <pmb0836@gmail.com>
2022-11-22 03:13:10 +0900

    Issues #74 fix: getGifticon 반환 값을 Flow 로 변경

--- data/src/main/java/com/lighthouse/database/dao/GifticonDao.kt
+++ data/src/main/java/com/lighthouse/database/dao/GifticonDao.kt
@@ -4,10 +4,11 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.lighthouse.database.entity.GifticonEntity
 import com.lighthouse.database.entity.GifticonEntity.Companion.GIFTICON_TABLE
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GifticonDao {
 
     @Query("SELECT * FROM $GIFTICON_TABLE WHERE id = :id")
-    fun getGifticon(id: String): GifticonEntity
+    fun getGifticon(id: String): Flow<GifticonEntity>
 }
