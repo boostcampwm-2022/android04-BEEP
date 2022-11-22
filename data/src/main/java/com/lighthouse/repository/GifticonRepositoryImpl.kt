837ea4b49e37da7eea0bbe2e71ff7f6d2b7ffdff
mangbaam <pmb0836@gmail.com>
2022-11-23 02:26:05 +0900

    Issues #76 feat: 기프티콘 사용 처리 Room 로직 작성

--- data/src/main/java/com/lighthouse/repository/GifticonRepositoryImpl.kt
+++ data/src/main/java/com/lighthouse/repository/GifticonRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.lighthouse.repository
 
 import com.lighthouse.database.mapper.toGifticonEntity
+import com.lighthouse.database.mapper.toUsageHistoryEntity
 import com.lighthouse.datasource.gifticon.GifticonLocalDataSource
 import com.lighthouse.domain.model.DbResult
 import com.lighthouse.domain.model.Gifticon
+import com.lighthouse.domain.model.UsageHistory
 import com.lighthouse.domain.repository.GifticonRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -29,5 +31,30 @@ class GifticonRepositoryImpl @Inject constructor(
                 it.toGifticonEntity()
             }
         )
+    }
+
+    override fun getUsageHistory(gifticonId: String): Flow<DbResult<List<UsageHistory>>> = flow {
+        emit(DbResult.Loading)
+        gifticonLocalDataSource.getUsageHistory(gifticonId).collect {
+            emit(DbResult.Success(it))
+        }
+    }.catch { e ->
+        emit(DbResult.Failure(e))
+    }
+
+    override suspend fun saveUsageHistory(gifticonId: String, usageHistory: UsageHistory) {
+        gifticonLocalDataSource.insertUsageHistory(usageHistory.toUsageHistoryEntity(gifticonId))
+    }
+
+    override suspend fun useGifticon(gifticonId: String, usageHistory: UsageHistory) {
+        gifticonLocalDataSource.useGifticon(usageHistory.toUsageHistoryEntity(gifticonId))
+    }
+
+    override suspend fun useCashCardGifticon(gifticonId: String, amount: Int, usageHistory: UsageHistory) {
+        gifticonLocalDataSource.useCashCardGifticon(amount, usageHistory.toUsageHistoryEntity(gifticonId))
+    }
+
+    override suspend fun unUseGifticon(gifticonId: String) {
+        gifticonLocalDataSource.unUseGifticon(gifticonId)
     }
 }
--- domain/src/main/java/com/lighthouse/domain/repository/GifticonRepository.kt
+++ domain/src/main/java/com/lighthouse/domain/repository/GifticonRepository.kt
@@ -2,12 +2,17 @@ package com.lighthouse.domain.repository
 
 import com.lighthouse.domain.model.DbResult
 import com.lighthouse.domain.model.Gifticon
+import com.lighthouse.domain.model.UsageHistory
 import kotlinx.coroutines.flow.Flow
 
 interface GifticonRepository {
 
     fun getGifticon(id: String): Flow<DbResult<Gifticon>>
     suspend fun saveGifticons(gifticons: List<Gifticon>)
-//  TODO: fun getGifticonSharedDate(id: String): Result<Flow<Date?>>
-//  TODO: fun getGifticonUsageHistory(id: String): Result<Flow<List<UsageHistory>>>
+
+    fun getUsageHistory(gifticonId: String): Flow<DbResult<List<UsageHistory>>>
+    suspend fun saveUsageHistory(gifticonId: String, usageHistory: UsageHistory)
+    suspend fun useGifticon(gifticonId: String, usageHistory: UsageHistory)
+    suspend fun useCashCardGifticon(gifticonId: String, amount: Int, usageHistory: UsageHistory)
+    suspend fun unUseGifticon(gifticonId: String)
 }
