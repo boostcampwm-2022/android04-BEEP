3863eb1ba9615a68a2ff1b88dfd8dcc54a187d24
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-22 19:47:26 +0900

    Issues #72 feat: navermap 현재 위치 추적 버튼 구현

--- presentation/src/main/java/com/lighthouse/presentation/ui/map/MapActivity.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/map/MapActivity.kt
@@ -1,11 +1,13 @@
 package com.lighthouse.presentation.ui.map
 
-import android.location.Location
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
 import com.lighthouse.domain.model.Gifticon
 import com.lighthouse.presentation.R
@@ -22,24 +24,28 @@ import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.widget.LocationButtonView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import java.util.Date
 import java.util.UUID
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickListener, OnLocationUpdateListener {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickListener {
 
     private lateinit var binding: ActivityMapBinding
     private lateinit var naverMap: NaverMap
     private lateinit var mapView: MapView
     private lateinit var fusedLocationProviderClient: FusedLocationProvider
+    private lateinit var client: FusedLocationProviderClient
     private lateinit var locationSource: FusedLocationSource
     private val viewModel: MapViewModel by viewModels()
     private val adapter = MapGifticonAdapter()
+    private val currentLocationButton: LocationButtonView by lazy { binding.btnCurrentLocation }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        client = LocationServices.getFusedLocationProviderClient(this)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
         mapView = binding.mapView.apply {
             onCreate(savedInstanceState)
@@ -92,21 +98,29 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickList
     }
 
     private fun setFusedLocationProvider() {
-        fusedLocationProviderClient = FusedLocationProvider(this, this).apply {
-            requestLastLocation()
-        }
+        fusedLocationProviderClient = FusedLocationProvider(this)
     }
 
     override fun onMapReady(map: NaverMap) {
         naverMap = map
         locationSource = FusedLocationSource(this, 1000)
         naverMap.locationSource = locationSource
+
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = false
+
+        currentLocationButton.map = naverMap
         setNaverMapZoom()
     }
 
+    @SuppressLint("MissingPermission")
     private fun setNaverMapZoom() {
         naverMap.maxZoom = 18.0
         naverMap.minZoom = 10.0
+
+        client.lastLocation.addOnSuccessListener { startLocation ->
+            moveMapCamera(startLocation.longitude, startLocation.latitude)
+        }
     }
 
     private fun setGifticonAdapter() {
@@ -123,11 +137,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickList
 
     override fun onClick(overlay: Overlay): Boolean {
         return true
-    }
-
-    override fun onLocationUpdated(location: Location) {
-        viewModel.getBrandPlaceInfos(location.longitude, location.latitude)
-        moveMapCamera(location.longitude, location.latitude)
     }
 
     private fun moveMapCamera(longitude: Double, latitude: Double) {
@@ -157,7 +166,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickList
 
     override fun onDestroy() {
         mapView.onDestroy()
-        fusedLocationProviderClient.stopLocationUpdates()
         super.onDestroy()
     }
 
