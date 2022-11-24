43e7d6483fba33ef627c01e164ee285a2f3610b4
김명석 <audxo112@naver.com>
2022-11-24 14:21:45 +0900

    Issues #82 feat: contentUri 에서 Thumbnail을 가져오는 코드 구현

--- presentation/src/main/java/com/lighthouse/presentation/binding/ImageView.kt
+++ presentation/src/main/java/com/lighthouse/presentation/binding/ImageView.kt
@@ -4,12 +4,27 @@ import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.lighthouse.presentation.extension.getThumbnail
 
 @BindingAdapter("loadUri")
 fun loadUri(view: ImageView, uri: Uri?) {
     if (uri != null) {
         Glide.with(view)
             .load(uri)
+            .centerCrop()
+            .into(view)
+    } else {
+        view.setImageBitmap(null)
+    }
+}
+
+@BindingAdapter("loadThumbnailByContentUri")
+fun loadThumbnailByContentUri(view: ImageView, contentUri: Uri?) {
+    val resolver = view.context.contentResolver
+    val thumbnail = resolver.getThumbnail(contentUri)
+    if (thumbnail != null) {
+        Glide.with(view)
+            .load(thumbnail)
             .centerCrop()
             .into(view)
     } else {
