265a8b07208c08172a1dce0c479b4520acaf1823
김명석 <audxo112@naver.com>
2022-11-27 02:24:31 +0900

    Issues #82 feat: CropImage 에 Init Crop Rect 설정 기능 추가

--- presentation/src/main/java/com/lighthouse/presentation/ui/cropgifticon/view/CropImageView.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/cropgifticon/view/CropImageView.kt
@@ -12,7 +12,6 @@ import android.graphics.Paint
 import android.graphics.PointF
 import android.graphics.RectF
 import android.graphics.Region
-import android.net.Uri
 import android.os.Build
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -178,60 +177,65 @@ class CropImageView(context: Context, attrs: AttributeSet?) : View(context, attr
         this.onCropImageListener = onCropImageListener
     }
 
-    fun setOriginUri(uri: Uri?) {
-        originBitmap = when (uri?.scheme) {
-            SCHEME_CONTENT -> context.contentResolver.getBitmap(uri)
-            SCHEME_FILE -> BitmapFactory.decodeFile(uri.path)
+    fun setCropInfo(info: CropImageInfo) {
+        originBitmap = when (info.uri?.scheme) {
+            SCHEME_CONTENT -> context.contentResolver.getBitmap(info.uri)
+            SCHEME_FILE -> BitmapFactory.decodeFile(info.uri.path)
             else -> null
         }
 
-        initRect()
+        initRect(info.croppedRect)
     }
 
     fun cropImage() {
         val bitmap = originBitmap
         if (bitmap != null) {
-            val rect = RectF(curCropRect)
+            val croppedRect = RectF(curCropRect)
             mainMatrix.invert(mainInverseMatrix)
-            mainInverseMatrix.mapRect(rect)
+            mainInverseMatrix.mapRect(croppedRect)
 
             val croppedBitmap = Bitmap.createBitmap(
                 bitmap,
-                rect.left.toInt(),
-                rect.top.toInt(),
-                (rect.right - rect.left).toInt(),
-                (rect.bottom - rect.top).toInt()
+                croppedRect.left.toInt(),
+                croppedRect.top.toInt(),
+                (croppedRect.right - croppedRect.left).toInt(),
+                (croppedRect.bottom - croppedRect.top).toInt()
             )
-            onCropImageListener?.onCrop(croppedBitmap)
+            onCropImageListener?.onCrop(croppedBitmap, croppedRect)
         } else {
-            onCropImageListener?.onCrop(null)
+            onCropImageListener?.onCrop(null, null)
         }
     }
 
     // 새로운 이미지 등록시, Rect 초기화
-    private fun initRect() {
+    private fun initRect(croppedRect: RectF? = null) {
         mainMatrix.reset()
 
         val bitmap = originBitmap
         if (bitmap != null) {
             realImageRect.set(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat())
             curImageRect.set(realImageRect)
-            if (aspectRatioEnable) {
-                // AspectRatio 에 맞춰서 CropRect 를 변경 한다
-                val aspectWidth = min(realImageRect.width(), realImageRect.height() * aspectRatio)
-                val aspectHeight = min(realImageRect.width() / aspectRatio, realImageRect.height())
 
-                val aspectOffsetX = (realImageRect.width() - aspectWidth) / 2
-                val aspectOffsetY = (realImageRect.height() - aspectHeight) / 2
-
-                curCropRect.set(
-                    aspectOffsetX,
-                    aspectOffsetY,
-                    aspectOffsetX + aspectWidth,
-                    aspectOffsetY + aspectHeight
-                )
+            if (croppedRect != null && croppedRect != RECT_F_EMPTY) {
+                curCropRect.set(croppedRect)
             } else {
-                curCropRect.set(realImageRect)
+                if (aspectRatioEnable) {
+                    // AspectRatio 에 맞춰서 CropRect 를 변경 한다
+                    val aspectWidth = min(realImageRect.width(), realImageRect.height() * aspectRatio)
+                    val aspectHeight = min(realImageRect.width() / aspectRatio, realImageRect.height())
+
+                    val aspectOffsetX = (realImageRect.width() - aspectWidth) / 2
+                    val aspectOffsetY = (realImageRect.height() - aspectHeight) / 2
+
+                    curCropRect.set(
+                        aspectOffsetX,
+                        aspectOffsetY,
+                        aspectOffsetX + aspectWidth,
+                        aspectOffsetY + aspectHeight
+                    )
+                } else {
+                    curCropRect.set(realImageRect)
+                }
             }
         } else {
             realImageRect.set(RECT_F_EMPTY)
