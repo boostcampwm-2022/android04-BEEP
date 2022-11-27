c12347e2671feb7fbbacd84f2f974ac00f41945c
mangbaam <pmb0836@gmail.com>
2022-11-28 01:39:32 +0900

    Issues #110 feat: 최대 한계를 넘지 않고, 숫자 형식을 유지하는 ConcurrencyEditText 작성
    - 텍스트 수정 시 커서는 맨 끝으로 간다
    - 콤마를 지우지 못한다 (맨 뒤로 이동)
    - 최대 크기를 지정하고 싶다면 xml 에서 `app:maxValue` 에 값을 주면 된다

--- presentation/src/main/java/com/lighthouse/presentation/ui/common/ConcurrencyEditText.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/common/ConcurrencyEditText.kt
@@ -2,12 +2,14 @@ package com.lighthouse.presentation.ui.common
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.text.Editable
-import android.text.TextWatcher
+import android.text.InputFilter
 import android.util.AttributeSet
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.lighthouse.presentation.R
+import timber.log.Timber
+import java.text.DecimalFormat
 
 class ConcurrencyEditText @JvmOverloads constructor(
     context: Context,
@@ -26,25 +28,19 @@ class ConcurrencyEditText @JvmOverloads constructor(
             setTextChangedListener()
             field = value
         }
+    private lateinit var unitText: CharSequence // 화폐 구분자. ex) 1,000,000 에서 콤마(,) 를 의미함
+    private lateinit var numberFormat: DecimalFormat
+
+    var text = ""
+        private set
+    var value = 0
+        private set
 
     init {
         val binding = inflate(context, R.layout.custom_concurrency_edit_text, this)
         inputEditText = binding.findViewById(R.id.tiet_value)
-        initAttrs()
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ConcurrencyEditText)
         setTypeArray(typedArray)
-    }
-
-    private fun initAttrs() {
-        suffixText = context.getString(R.string.all_cash_origin_unit)
-        setSuffixTextAppearance(R.style.BEEP_TextStyle_H5)
-        isHelperTextEnabled = false
-        isHintEnabled = false
-        boxBackgroundColor = context.getColor(android.R.color.transparent)
-
-        with(inputEditText) {
-            setTextAppearance(R.style.BEEP_TextStyle_H5)
-        }
     }
 
     private fun setTypeArray(typedArray: TypedArray) {
@@ -56,11 +52,16 @@ class ConcurrencyEditText @JvmOverloads constructor(
 
         maxValue = typedArray.getInt(R.styleable.ConcurrencyEditText_maxValue, Int.MAX_VALUE)
 
+        // 화폐 단위를 쪼갤 크기. ex) 1,000,000 일 때 chunkSize 는 3
         chunkSize = typedArray.getInt(R.styleable.ConcurrencyEditText_maxValue, 3)
 
-        val unitText = typedArray.getText(R.styleable.ConcurrencyEditText_unitText)
-        suffixText = unitText
+        // 화폐 구분자. ex) 1,000,000 에서 콤마(,) 를 의미함
+        unitText = typedArray.getText(R.styleable.ConcurrencyEditText_unitText) ?: ","
 
+        // 숫자 포맷. ex) 1,000,000 과 같이 나타낼 때 #,### 으로 만듦
+        numberFormat = DecimalFormat("#$unitText" + "#".repeat(chunkSize))
+
+        // 밑줄 색상
         val underStrokeColor = typedArray.getColor(R.styleable.ConcurrencyEditText_underStrokeColor, 0)
         boxStrokeColor = underStrokeColor
 
@@ -68,26 +69,39 @@ class ConcurrencyEditText @JvmOverloads constructor(
     }
 
     private fun setTextChangedListener() {
-        inputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                /*val number = convertToNumber(s?.toString() ?: return)
-                if (number.isNotBlank()) {
-                    inputEditText.setText(number.chunked(chunkSize).joinToString(","))
+        inputEditText.filters = arrayOf(
+            InputFilter { source, start, end, dest, dstart, dend ->
+                Timber.tag("concurrency").d("source: $source, start: $start, end: $end, dest: $dest, dstart: $dstart, dend: $dend")
+                val totalString = dest.substring(0 until dstart) + source + dest.substring(dend)
+                val number = convertToNumber(totalString)
+                if ((dest.toString().isNotBlank() && number.isBlank()) || (number.isNotBlank() && number.toLong() > maxValue)) {
+                    Timber.tag("concurrency").d("source: $source")
+                    return@InputFilter ""
                 } else {
-                    inputEditText.setText("")
-                }*/
+                    return@InputFilter null
+                }
             }
-        })
+        )
+        inputEditText.doOnTextChanged { s, _, _, _ ->
+            val newString = s?.toString() ?: ""
+            if (text == newString) return@doOnTextChanged
+
+            val number = convertToNumber(newString)
+            if (number.isBlank()) return@doOnTextChanged
+
+            text = numberFormat.format(number.toInt())
+            inputEditText.setText(text.ifBlank { EMPTY_TEXT })
+            inputEditText.setSelection(text.length) // 커서를 맨 뒤로 설정
+            Timber.tag("concurrency").d("text: $text")
+        }
     }
 
     private fun convertToNumber(text: String): String {
         val number = text.filter { it.isDigit() }.toIntOrNull()
         return number?.toString() ?: ""
+    }
+
+    companion object {
+        const val EMPTY_TEXT = "0"
     }
 }
