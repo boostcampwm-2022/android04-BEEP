591f7b5168e811f4c15566acaa4d2a1857dfe5ea
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-28 17:03:06 +0900

    Issues #108 feat: 테스트 데이터 추가

--- app/src/main/java/com/lighthouse/BeepApplication.kt
+++ app/src/main/java/com/lighthouse/BeepApplication.kt
@@ -2,16 +2,44 @@ package com.lighthouse
 
 import android.app.Application
 import com.lighthouse.beep.BuildConfig
+import com.lighthouse.domain.model.Gifticon
+import com.lighthouse.domain.repository.GifticonRepository
+import com.lighthouse.presentation.util.UUID
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.Date
+import javax.inject.Inject
 
 @HiltAndroidApp
 class BeepApplication : Application() {
+
+    @Inject
+    lateinit var repository: GifticonRepository
+
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(CustomTimberTree())
+        }
+
+        // TODO 테스트 데이터 삽입
+        val gifticonTestData = listOf(
+            Gifticon(UUID.generate(), "이름", "스타벅스", "스타벅스", Date(120, 20, 20), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "이름", "베스킨라빈스", Date(122, 11, 15), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "BHC", "BHC", Date(122, 5, 10), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "GS25", "GS25", Date(160, 10, 20), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "CU", "CU", Date(160, 10, 20), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "서브웨이", "서브웨이", Date(160, 10, 20), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "세븐일레븐", "세븐일레븐", Date(160, 10, 20), "bar", true, 1, "memo", true),
+            Gifticon(UUID.generate(), "이름", "파파존스", "파파존스", Date(160, 10, 20), "bar", true, 1, "memo", true)
+        )
+
+        CoroutineScope(Dispatchers.Main).launch {
+            repository.saveGifticons(gifticonTestData)
         }
     }
 }
