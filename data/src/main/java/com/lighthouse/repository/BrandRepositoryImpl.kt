411b6e9c94238ac7c5a033b9b88df61d380a7836
lee-ji-hoon <dlwlgns1240@naver.com>
2022-11-29 23:12:49 +0900

    Issues #131 feat: 필요없는 로직 및 데이터 삭제

--- data/src/main/java/com/lighthouse/repository/BrandRepositoryImpl.kt
+++ data/src/main/java/com/lighthouse/repository/BrandRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.lighthouse.repository
 import com.lighthouse.datasource.brand.BrandLocalDataSource
 import com.lighthouse.datasource.brand.BrandRemoteDataSource
 import com.lighthouse.domain.Dms
-import com.lighthouse.domain.DmsLocation
 import com.lighthouse.domain.model.BrandPlaceInfo
 import com.lighthouse.domain.repository.BrandRepository
 import com.lighthouse.mapper.toDomain
@@ -26,21 +25,6 @@ class BrandRepositoryImpl @Inject constructor(
             brandLocalSource.getBrands(x, y, brandName).mapCatching { it.toDomain() }.getOrDefault(emptyList())
         }
 
-    override suspend fun getNearByBrands(brandName: String, cardinalLocations: List<DmsLocation>): Result<Boolean> {
-        cardinalLocations.forEach { location ->
-            val isNearBrand = brandLocalSource.isNearBrand(location.x, location.y, brandName)
-            if (isNearBrand == null) {
-                val result = getRemoteSourceData(brandName, location.x, location.y, DEFAULT_SIZE).exceptionOrNull()
-                if (result is BeepErrorData) {
-                    return Result.failure(result.toDomain())
-                }
-            } else if (isNearBrand.isNotEmpty()) {
-                return Result.success(true)
-            }
-        }
-        return Result.success(false)
-    }
-
     private suspend fun getRemoteSourceData(
         brandName: String,
         x: Dms,
@@ -55,9 +39,5 @@ class BrandRepositoryImpl @Inject constructor(
         } else {
             result.onSuccess { brandLocalSource.insertBrands(it, x, y, brandName) }
         }
-    }
-
-    companion object {
-        private const val DEFAULT_SIZE = 15
     }
 }
