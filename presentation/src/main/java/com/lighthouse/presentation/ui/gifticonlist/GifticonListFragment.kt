9a0dad8a072441c396a86db76e5ecfe7075beecc
mangbaam <pmb0836@gmail.com>
2022-11-29 19:36:22 +0900

    Issues #121 style: 상세화면 테스트 버튼 제거

--- presentation/src/main/java/com/lighthouse/presentation/ui/gifticonlist/GifticonListFragment.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/gifticonlist/GifticonListFragment.kt
@@ -1,6 +1,5 @@
 package com.lighthouse.presentation.ui.gifticonlist
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.compose.ui.platform.ViewCompositionStrategy
@@ -8,9 +7,7 @@ import androidx.fragment.app.Fragment
 import com.google.accompanist.appcompattheme.AppCompatTheme
 import com.lighthouse.presentation.R
 import com.lighthouse.presentation.databinding.FragmentGifticonListBinding
-import com.lighthouse.presentation.extra.Extras.KEY_GIFTICON_ID
 import com.lighthouse.presentation.ui.common.viewBindings
-import com.lighthouse.presentation.ui.detailgifticon.GifticonDetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,14 +18,6 @@ class GifticonListFragment : Fragment(R.layout.fragment_gifticon_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO (테스트 용 코드 제거)
-        binding.btnTest.setOnClickListener {
-            startActivity(
-                Intent(requireContext(), GifticonDetailActivity::class.java).apply {
-                    putExtra(KEY_GIFTICON_ID, "sample1")
-                }
-            )
-        }
         binding.cvContainer.apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
