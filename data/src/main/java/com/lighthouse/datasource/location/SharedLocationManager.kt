dd1ffd0b08c303f1f4c3573a03ad62c405ccafb6
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-02 11:36:34 +0900

    Issues #143 feat: Location callbackFlow 수정
    - 기존에 보기 힘들었던 버전 분기가 필요 없다고 느껴져 변경
    - 권한이 변경됐다고 함수가 실행이 된다면 callbackFlow 실행

--- data/src/main/java/com/lighthouse/datasource/location/SharedLocationManager.kt
+++ data/src/main/java/com/lighthouse/datasource/location/SharedLocationManager.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Looper
 import androidx.core.app.ActivityCompat
@@ -12,13 +13,16 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.lighthouse.domain.Dms
+import com.lighthouse.domain.DmsLocation
+import com.lighthouse.domain.LocationConverter
 import com.lighthouse.domain.VertexLocation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.shareIn
 import timber.log.Timber
 
@@ -26,66 +30,88 @@ import timber.log.Timber
 class SharedLocationManager constructor(
     private val context: Context
 ) {
-    val receivingLocationUpdates = flow {
-        val result = context.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
-            context.hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-
-        emit(result)
-    }
+    var receivingLocationUpdates = MutableStateFlow(hasLocationPermission())
+        private set
 
     private val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private val locationRequest = LocationRequest.create().apply {
+        interval = LOCATION_INTERVAL
+        fastestInterval = LOCATION_INTERVAL / 2
+        priority = Priority.PRIORITY_HIGH_ACCURACY
+        maxWaitTime = LOCATION_INTERVAL
+    }
+    private var locationCallback: LocationCallback? = null
+    private var prevLocation = DmsLocation(Dms(0, 0, 0), Dms(0, 0, 0))
 
     private val locationUpdates = callbackFlow {
-        val locationRequest = LocationRequest.create().apply {
-            interval = LOCATION_INTERVAL
-            fastestInterval = LOCATION_INTERVAL / 2
-            priority = Priority.PRIORITY_HIGH_ACCURACY
-            maxWaitTime = LOCATION_INTERVAL
-        }
-
-        val locationCallback = object : LocationCallback() {
+        locationCallback = object : LocationCallback() {
             override fun onLocationResult(result: LocationResult) {
-                val lastLocationResult = result.lastLocation ?: return
-                Timber.tag("TAG").d("${javaClass.simpleName} lastLocationResult -> $lastLocationResult")
-                trySend(VertexLocation(lastLocationResult.longitude, lastLocationResult.latitude))
+                val locationResult = result.lastLocation ?: return
+                val currentLocation = setDmsLocation(locationResult)
+                Timber.tag("TAG").d("${javaClass.simpleName} lastLocationResult -> $locationResult")
+                if (prevLocation != currentLocation) {
+                    prevLocation = currentLocation
+                    Timber.tag("TAG").d("${javaClass.simpleName} 로케이션 갱신 $prevLocation")
+                    trySend(VertexLocation(locationResult.longitude, locationResult.latitude))
+                }
             }
         }
 
-        if (context.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION).not() &&
-            context.hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION).not()
-        ) close()
+        if (hasLocationPermission().not()) close()
 
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        ).addOnFailureListener { e ->
-            Timber.tag("TAG").d("${javaClass.simpleName} onFailure Location $e")
-            close(e)
-        }.addOnSuccessListener {
-            Timber.tag("TAG").d("${javaClass.simpleName} Start Location Updates")
-        }
+        setProviderRequest()
 
         awaitClose {
             Timber.tag("TAG").d("${javaClass.simpleName} Stopping Location Updates")
-            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            locationCallback?.let { fusedLocationProviderClient.removeLocationUpdates(it) }
         }
     }.shareIn(CoroutineScope(Dispatchers.IO), SharingStarted.WhileSubscribed())
 
+    private fun hasLocationPermission() =
+        context.hasLocationPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
+            context.hasLocationPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    private fun Context.hasLocationPermission(permission: String): Boolean {
+        if (permission == Manifest.permission.ACCESS_BACKGROUND_LOCATION &&
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.Q
+        ) {
+            return true
+        }
+
+        return ActivityCompat.checkSelfPermission(this, permission) ==
+            PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun setProviderRequest() {
+        locationCallback?.let {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                it,
+                Looper.getMainLooper()
+            ).addOnFailureListener { e ->
+                receivingLocationUpdates.value = false
+                Timber.tag("TAG").d("${javaClass.simpleName} onFailure Location $e")
+            }.addOnSuccessListener {
+                receivingLocationUpdates.value = true
+                Timber.tag("TAG").d("${javaClass.simpleName} Start Location Updates")
+            }
+        }
+    }
+
+    private fun setDmsLocation(lastLocationResult: Location): DmsLocation {
+        val x = LocationConverter.toMinDms(lastLocationResult.longitude)
+        val y = LocationConverter.toMinDms(lastLocationResult.latitude)
+        return DmsLocation(x, y)
+    }
+
     fun locationFlow() = locationUpdates
+
+    fun changePermission(hasPermission: Boolean) {
+        receivingLocationUpdates.value = hasPermission
+        if (hasPermission) setProviderRequest()
+    }
 
     companion object {
         private const val LOCATION_INTERVAL = 30000L
     }
-}
-
-fun Context.hasPermission(permission: String): Boolean {
-    if (permission == Manifest.permission.ACCESS_BACKGROUND_LOCATION &&
-        Build.VERSION.SDK_INT < Build.VERSION_CODES.Q
-    ) {
-        return true
-    }
-
-    return ActivityCompat.checkSelfPermission(this, permission) ==
-        PackageManager.PERMISSION_GRANTED
 }
