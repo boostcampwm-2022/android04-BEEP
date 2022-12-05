6c3069f158351b19e1cb456432b1511226623311
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-06 02:04:55 +0900

    Issues #117 style: widget 백그라운드 / 반복되는 composable 분리

---
+++ presentation/src/main/java/com/lighthouse/presentation/ui/widget/component/GlanceKtx.kt
@@ -0,0 +1,56 @@
+package com.lighthouse.presentation.ui.widget.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.ColumnScope
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import com.lighthouse.presentation.R
+
+@Composable
+fun AppWidgetBox(
+    modifier: GlanceModifier = GlanceModifier,
+    contentAlignment: Alignment = Alignment.TopStart,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = appWidgetBackgroundModifier().then(modifier),
+        contentAlignment = contentAlignment,
+        content = content
+    )
+}
+
+@Composable
+fun AppWidgetColumn(
+    modifier: GlanceModifier = GlanceModifier,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    horizonAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = appWidgetBackgroundModifier().then(modifier),
+        verticalAlignment = verticalAlignment,
+        horizontalAlignment = horizonAlignment,
+        content = content
+    )
+}
+
+@Composable
+fun appWidgetBackgroundModifier() = GlanceModifier
+    .fillMaxSize()
+    .padding(16.dp)
+    .appWidgetBackground()
+    .background(R.color.widget_background)
+    .appWidgetBackgroundCornerRadius()
+
+fun GlanceModifier.appWidgetBackgroundCornerRadius(): GlanceModifier {
+    cornerRadius(16.dp)
+    return this
+}
