677399064b850709d5f7edebd1efd4536f8d9cde
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-06 20:56:44 +0900

    Issues #117 feat: 위짓 Receiver 및 worker 등록 변경
    - 새로고침했을때 worker를 재등록하는 것으로 변경
    - WorkManager에서 enqueue, cancel 코드 추가

--- presentation/src/main/java/com/lighthouse/presentation/background/BeepWorkManager.kt
+++ presentation/src/main/java/com/lighthouse/presentation/background/BeepWorkManager.kt
@@ -2,9 +2,6 @@ package com.lighthouse.presentation.background
 
 import android.content.Context
 import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -12,25 +9,56 @@ import com.lighthouse.presentation.ui.widget.BeepWidgetWorker
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.concurrent.TimeUnit
 
-class BeepWorkManager(@ApplicationContext context: Context) {
+class BeepWorkManager(
+    @ApplicationContext context: Context
+) {
+
+    private val workPolicyKeep = ExistingPeriodicWorkPolicy.KEEP
+    private val workPolicyReplace = ExistingPeriodicWorkPolicy.REPLACE
 
     private val notificationWorkRequest: PeriodicWorkRequest =
         PeriodicWorkRequestBuilder<NotificationWorker>(NOTIFICATION_INTERVAL, TimeUnit.MINUTES).build()
 
-    private val widgetWorkRequest: OneTimeWorkRequest =
-        OneTimeWorkRequestBuilder<BeepWidgetWorker>().build()
+    private val widgetWorkRequest: PeriodicWorkRequest =
+        PeriodicWorkRequestBuilder<BeepWidgetWorker>(WIDGET_INTERVAL, TimeUnit.MINUTES).build()
+
+    private val manager = WorkManager.getInstance(context)
 
     init {
-        WorkManager.getInstance(context)
-            .enqueueUniquePeriodicWork(NOTIFICATION_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, notificationWorkRequest)
+        manager.enqueueUniquePeriodicWork(
+            NOTIFICATION_WORK_NAME,
+            workPolicyKeep,
+            notificationWorkRequest
+        )
+    }
 
-        WorkManager.getInstance(context)
-            .enqueueUniqueWork(WIDGET_WORK_NAME, ExistingWorkPolicy.REPLACE, widgetWorkRequest)
+    /**
+     * 새로고침이나 시작할때 실행할 함수
+     * @param force 만약 새로고침
+     */
+    fun enqueue(force: Boolean = false) {
+        val policy = when (force) {
+            true -> workPolicyReplace
+            false -> workPolicyKeep
+        }
+        manager.enqueueUniquePeriodicWork(
+            WIDGET_WORK_NAME,
+            policy,
+            widgetWorkRequest
+        )
+    }
+
+    /**
+     * WidgetReceiver에서 onDisabled때 실행할 함수
+     */
+    fun cancel() {
+        manager.cancelUniqueWork(WIDGET_WORK_NAME)
     }
 
     companion object {
         private const val NOTIFICATION_WORK_NAME = "notification"
-        private const val WIDGET_WORK_NAME = "widget"
         private const val NOTIFICATION_INTERVAL = 15L // test를 위해 최소 간격인 15분으로 설정. 원래는 24시간.
+        private const val WIDGET_WORK_NAME = "widget"
+        private const val WIDGET_INTERVAL = 10L
     }
 }
