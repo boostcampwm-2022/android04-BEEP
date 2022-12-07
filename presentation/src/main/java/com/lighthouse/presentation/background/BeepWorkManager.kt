42114a7cf8b97288698d11d8ce1ac3057f59dc64
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-07 18:17:48 +0900

    Issues #117 feat: 리뷰반영 : WorkManager 싱글톤 인스턴스로 구현

--- presentation/src/main/java/com/lighthouse/presentation/background/BeepWorkManager.kt
+++ presentation/src/main/java/com/lighthouse/presentation/background/BeepWorkManager.kt
@@ -58,5 +58,15 @@ class BeepWorkManager(
         private const val NOTIFICATION_INTERVAL = 15L // test를 위해 최소 간격인 15분으로 설정. 원래는 24시간.
         private const val WIDGET_WORK_NAME = "widget"
         private const val WIDGET_INTERVAL = 15L
+
+        @Volatile
+        private var instance: BeepWorkManager? = null
+
+        fun getInstance(context: Context): BeepWorkManager =
+            instance ?: synchronized(this) {
+                BeepWorkManager(context).also {
+                    instance = it
+                }
+            }
     }
 }
