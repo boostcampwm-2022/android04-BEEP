72e58e529bf7534f853c3a85d18b2540d9b3b6f2
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-12-07 18:22:15 +0900

    Issues #120 refactor: 설정 저장 여부 판단 Usecase 추가

--- presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/signin/SignInViewModel.kt
@@ -3,22 +3,26 @@ package com.lighthouse.presentation.ui.signin
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lighthouse.domain.model.UserPreferenceOption
-import com.lighthouse.domain.repository.UserPreferencesRepository
+import com.lighthouse.domain.usecase.setting.GetGuestOptionUseCase
+import com.lighthouse.domain.usecase.setting.GetOptionStoredUseCase
+import com.lighthouse.domain.usecase.setting.SaveGuestOptionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val userPreferencesRepository: UserPreferencesRepository
+    getOptionStoredUseCase: GetOptionStoredUseCase,
+    getGuestOptionUseCase: GetGuestOptionUseCase,
+    private val saveGuestOptionUseCase: SaveGuestOptionUseCase
 ) : ViewModel() {
 
-    val isGuestStored = userPreferencesRepository.isStored(UserPreferenceOption.GUEST)
-    val isGuest = userPreferencesRepository.getBooleanOption(UserPreferenceOption.GUEST)
+    val isGuestStored = getOptionStoredUseCase(UserPreferenceOption.GUEST)
+    val isGuest = getGuestOptionUseCase()
 
     fun saveGuestOption(option: Boolean) {
         viewModelScope.launch {
-            userPreferencesRepository.setBooleanOption(UserPreferenceOption.GUEST, option)
+            saveGuestOptionUseCase(option)
         }
     }
 }
