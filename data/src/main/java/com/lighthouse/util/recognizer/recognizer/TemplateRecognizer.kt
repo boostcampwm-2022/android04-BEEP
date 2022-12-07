b8005d3e49a8b016399a07f732daf21f59c8c113
김명석 <audxo112@naver.com>
2022-12-07 21:49:24 +0900

    Issues #162 feat: TemplateRecognizer 구현

---
+++ data/src/main/java/com/lighthouse/util/recognizer/recognizer/TemplateRecognizer.kt
@@ -0,0 +1,30 @@
+package com.lighthouse.util.recognizer.recognizer
+
+import android.graphics.Bitmap
+import com.lighthouse.util.recognizer.GifticonRecognizeInfo
+import com.lighthouse.util.recognizer.parser.BaseParser
+import com.lighthouse.util.recognizer.processor.BaseProcessor
+
+abstract class TemplateRecognizer {
+
+    protected abstract val parser: BaseParser
+
+    protected abstract val processor: BaseProcessor
+
+    private val textRecognizer = TextRecognizer()
+
+    fun match(inputs: List<String>) = parser.match(inputs)
+
+    suspend fun recognize(bitmap: Bitmap, info: GifticonRecognizeInfo): GifticonRecognizeInfo {
+        var newInfo = parser.parseKeyword(info)
+        val result = processor.process(bitmap)
+
+        newInfo = newInfo.copy(croppedImage = result.image.bitmap, croppedRect = result.image.rect)
+        for (text in result.textList) {
+            val inputs = textRecognizer.recognize(text.bitmap)
+            text.bitmap.recycle()
+            newInfo = parser.parseText(newInfo, text.tag, inputs)
+        }
+        return newInfo
+    }
+}
---
+++ data/src/main/java/com/lighthouse/util/recognizer/recognizer/TextRecognizer.kt
@@ -0,0 +1,21 @@
+package com.lighthouse.util.recognizer.recognizer
+
+import android.graphics.Bitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+
+class TextRecognizer {
+    suspend fun recognize(bitmap: Bitmap) = callbackFlow {
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val recognition = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
+        recognition.process(image).addOnSuccessListener {
+            trySend(it.text.lines())
+            close()
+        }
+        awaitClose()
+    }.first()
+}
