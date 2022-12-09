6e1ec14b0d27b9cc710258a7b27ef0465c415df2
김명석 <audxo112@naver.com>
2022-12-10 00:42:44 +0900

    Issues #162 feat: AddGifticonUIModel 값 추가 및 Mapper 정리

---
+++ presentation/src/main/java/com/lighthouse/presentation/mapper/GalleryImageMapper.kt
@@ -0,0 +1,12 @@
+package com.lighthouse.presentation.mapper
+
+import android.net.Uri
+import com.lighthouse.domain.model.GalleryImage
+import com.lighthouse.presentation.model.GalleryUIModel
+
+fun GalleryImage.toPresentation(index: Int = -1): GalleryUIModel.Gallery = GalleryUIModel.Gallery(
+    id = id,
+    uri = Uri.parse(contentUri),
+    selectedOrder = index,
+    date = date
+)
