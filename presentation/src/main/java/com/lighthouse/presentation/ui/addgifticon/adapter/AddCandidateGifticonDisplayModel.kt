608d3d0bae60977281b7a50269d38c2475315403
김명석 <audxo112@naver.com>
2022-12-10 22:12:19 +0900

    Issues #162 style: Ripple Effect 추가

--- presentation/src/main/java/com/lighthouse/presentation/ui/addgifticon/adapter/AddCandidateGifticonDisplayModel.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/addgifticon/adapter/AddCandidateGifticonDisplayModel.kt
@@ -1,25 +1,16 @@
 package com.lighthouse.presentation.ui.addgifticon.adapter
 
-import com.lighthouse.presentation.R
-
 class AddCandidateGifticonDisplayModel(
     var item: AddGifticonItemUIModel.Gifticon,
     private val onClick: (AddGifticonItemUIModel.Gifticon) -> Unit,
     private val onDelete: (AddGifticonItemUIModel.Gifticon) -> Unit
 ) {
 
-    val badgeVisible
-        get() = when {
-            item.isDelete || item.isValid.not() -> true
-            else -> false
-        }
+    val deleteVisible
+        get() = item.isDelete
 
-    val badgeResId
-        get() = when {
-            item.isDelete -> R.drawable.ic_item_delete
-            item.isValid.not() -> R.drawable.ic_item_invalid
-            else -> null
-        }
+    val invalidVisible
+        get() = item.isDelete.not() && item.isValid.not()
 
     fun onClickItem() {
         if (item.isDelete) {
