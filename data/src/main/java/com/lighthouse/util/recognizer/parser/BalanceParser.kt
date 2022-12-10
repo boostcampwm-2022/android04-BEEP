d96c29e26ac78303a9e8f4583bd075ffc1945ea4
김명석 <audxo112@naver.com>
2022-12-10 15:33:41 +0900

    Issues #162 feat: 100원 이상의 금액 인식하게 수정

--- data/src/main/java/com/lighthouse/util/recognizer/parser/BalanceParser.kt
+++ data/src/main/java/com/lighthouse/util/recognizer/parser/BalanceParser.kt
@@ -2,7 +2,7 @@ package com.lighthouse.util.recognizer.parser
 
 class BalanceParser {
     private val cashCardFilterRegex = listOf(
-        CashCardRegex("(\\d{0,3}[,]?\\d{0,3}[,]\\d{1,3})".toRegex(), 1),
+        CashCardRegex("\\b(\\d{0,3}[,]?\\d{0,3}[,]\\d{1,3})".toRegex(), 1),
         CashCardRegex("(\\d+)천원".toRegex(), 1000),
         CashCardRegex("(\\d+)만원".toRegex(), 10000),
         CashCardRegex("(\\d+)십만원".toRegex(), 100000)
@@ -16,6 +16,6 @@ class BalanceParser {
             }
         } ?: 0
 
-        return BalanceParserResult(balance = balance)
+        return BalanceParserResult(balance = if (balance >= 100) balance else 0)
     }
 }
