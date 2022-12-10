be1944ae1c7e52bd69733a7894a650ed3a127014
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-12-10 18:45:23 +0900

    Issues #201 refactor: 필요없는 코드 삭제

--- presentation/src/main/java/com/lighthouse/presentation/ui/setting/UsedGifticonViewModel.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/setting/UsedGifticonViewModel.kt
@@ -5,12 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.lighthouse.domain.model.DbResult
 import com.lighthouse.domain.usecase.GetGifticonsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,13 +22,4 @@ class UsedGifticonViewModel @Inject constructor(
             emptyList()
         }
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
-
-    init {
-        viewModelScope.launch {
-            while (true) {
-                Timber.tag("USED").d("${usedGifticons.value}")
-                delay(1000L)
-            }
-        }
-    }
 }
