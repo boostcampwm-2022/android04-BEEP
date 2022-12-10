16366aeaccedfc15bcb4a9ce07e8758b43adcf63
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-12-10 21:24:53 +0900

    Issues #200 style: 도장 쾅 애니메이션

--- presentation/src/main/java/com/lighthouse/presentation/binding/View.kt
+++ presentation/src/main/java/com/lighthouse/presentation/binding/View.kt
@@ -1,7 +1,9 @@
 package com.lighthouse.presentation.binding
 
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.databinding.BindingAdapter
+import com.lighthouse.presentation.R
 import com.lighthouse.presentation.util.OnThrottleClickListener
 
 @BindingAdapter("isVisible")
@@ -16,4 +18,16 @@ fun View.setOnThrottleClickListener(listener: (View) -> Unit) {
             listener(view)
         }
     })
+}
+
+@BindingAdapter("isStamp")
+fun playStampAnimation(view: View, visible: Boolean) {
+    view.visibility = if (visible) View.VISIBLE else View.GONE
+    if (visible) {
+        val animation = AnimationUtils.loadAnimation(view.context, R.anim.anim_stamp)
+        view.startAnimation(animation)
+    } else {
+        val animation = AnimationUtils.loadAnimation(view.context, R.anim.anim_fade_out)
+        view.startAnimation(animation)
+    }
 }
