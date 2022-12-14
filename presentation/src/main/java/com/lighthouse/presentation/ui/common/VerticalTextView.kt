59b858f019006eb4b4e06ccabfd102b7d59f4795
mangbaam <pmb0836@gmail.com>
2022-12-15 02:15:50 +0900

    Issues #240 feat: 세로 방향으로 회전한 TextView 커스텀
    rotate 속성 만을 사용하면 layout 이 제대로 잡히지 않아서 위치를 지정하기 어려운 문제 때문에 작성

---
+++ presentation/src/main/java/com/lighthouse/presentation/ui/common/VerticalTextView.kt
@@ -0,0 +1,60 @@
+package com.lighthouse.presentation.ui.common
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import com.lighthouse.presentation.R
+
+/**
+ * TextView 를 세로로 회전한 뷰
+ *
+ * @property topDown true 일 때 시계 방향 회전, false 일 때 반시계 방향 회전
+ *
+ * 코드 참고: [stackoverflow.com/a/45414489](https://stackoverflow.com/a/45414489)
+ */
+class VerticalTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatTextView(context, attrs, defStyleAttr) {
+
+    var topDown: Boolean = DEFAULT_TOP_DOWN
+
+    init {
+        attrs?.let {
+            context.obtainStyledAttributes(it, R.styleable.VerticalTextView).run {
+                topDown = getBoolean(R.styleable.VerticalTextView_topDown, DEFAULT_TOP_DOWN)
+                recycle()
+            }
+        }
+    }
+
+    override fun onMeasure(
+        widthMeasureSpec: Int,
+        heightMeasureSpec: Int
+    ) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec)
+        setMeasuredDimension(measuredHeight, measuredWidth)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.save()
+        if (topDown) {
+            canvas.translate(width.toFloat(), 0f)
+            canvas.rotate(90f)
+        } else {
+            canvas.translate(0f, height.toFloat())
+            canvas.rotate(-90f)
+        }
+        canvas.translate(
+            compoundPaddingLeft.toFloat(),
+            extendedPaddingTop.toFloat()
+        )
+        layout.draw(canvas)
+        canvas.restore()
+    }
+
+    companion object {
+        private const val DEFAULT_TOP_DOWN = true
+    }
+}
