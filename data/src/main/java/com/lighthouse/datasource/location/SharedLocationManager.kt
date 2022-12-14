c945fbc1327d3477e8f5debd0786b044e3183748
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-14 16:05:58 +0900

    Issues #225 feat: location callbackFlow 변경

--- data/src/main/java/com/lighthouse/datasource/location/SharedLocationManager.kt
+++ data/src/main/java/com/lighthouse/datasource/location/SharedLocationManager.kt
@@ -14,13 +14,10 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.lighthouse.domain.VertexLocation
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.shareIn
+import timber.log.Timber
 import javax.inject.Inject
 
 @SuppressLint("MissingPermission")
@@ -36,26 +33,36 @@ class SharedLocationManager @Inject constructor(
         interval = LOCATION_INTERVAL
         fastestInterval = LOCATION_INTERVAL / 2
         priority = Priority.PRIORITY_HIGH_ACCURACY
-        maxWaitTime = LOCATION_INTERVAL
+        smallestDisplacement = SMALLEST_DISPLACEMENT_DIFF
+        maxWaitTime = WAITE_TIME
     }
-    private var locationCallback: LocationCallback? = null
 
     private val locationUpdates = callbackFlow {
-        locationCallback = object : LocationCallback() {
+        val locationCallback = object : LocationCallback() {
             override fun onLocationResult(result: LocationResult) {
+                Timber.tag("TAG").d("${javaClass.simpleName} result -> $result")
                 val locationResult = result.lastLocation ?: return
                 trySend(VertexLocation(locationResult.longitude, locationResult.latitude))
             }
         }
 
-        if (hasLocationPermission().not()) close()
-
-        setProviderRequest()
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        ).addOnFailureListener { e ->
+            receivingLocationUpdates.value = false
+            Timber.tag("TAG").d("${javaClass.simpleName} provider request Failure")
+        }.addOnSuccessListener {
+            Timber.tag("TAG").d("${javaClass.simpleName} provider request Success")
+            receivingLocationUpdates.value = true
+        }
 
         awaitClose {
-            locationCallback?.let { fusedLocationProviderClient.removeLocationUpdates(it) }
+            Timber.tag("TAG").d("${javaClass.simpleName} awaitClose")
+            locationCallback.let { fusedLocationProviderClient.removeLocationUpdates(it) }
         }
-    }.shareIn(CoroutineScope(Dispatchers.IO), SharingStarted.WhileSubscribed())
+    }
 
     private fun hasLocationPermission() =
         context.hasLocationPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
@@ -72,28 +79,15 @@ class SharedLocationManager @Inject constructor(
             PackageManager.PERMISSION_GRANTED
     }
 
-    private fun setProviderRequest() {
-        locationCallback?.let {
-            fusedLocationProviderClient.requestLocationUpdates(
-                locationRequest,
-                it,
-                Looper.getMainLooper()
-            ).addOnFailureListener { e ->
-                receivingLocationUpdates.value = false
-            }.addOnSuccessListener {
-                receivingLocationUpdates.value = true
-            }
-        }
-    }
-
     fun locationFlow() = locationUpdates
 
     fun changePermission(hasPermission: Boolean) {
         receivingLocationUpdates.value = hasPermission
-        if (hasPermission) setProviderRequest()
     }
 
     companion object {
-        private const val LOCATION_INTERVAL = 10000L
+        private const val LOCATION_INTERVAL = 30000L
+        private const val WAITE_TIME = 2000L
+        private const val SMALLEST_DISPLACEMENT_DIFF = 250F
     }
 }
