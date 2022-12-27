cdbdaa6032201841fbd621b145c3cd79f9383d9c
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-27 23:13:14 +0900

    Issues #26 fix: CD 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+        classpath(Libraries.google_services)
     }
 }
 
