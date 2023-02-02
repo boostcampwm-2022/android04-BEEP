0a18c052a0fa66e6e47572f97d50ac3484a3e852
lee-ji-hoon <dlwlgns1240@naver.com>
2023-02-02 15:01:24 +0900

    Issues #281 fix: gradle version
    - 7.4 -> 7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
