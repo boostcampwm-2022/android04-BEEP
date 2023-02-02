4b6230f2567a7995178950b3c6242f5d6a4149f8
lee-ji-hoon <dlwlgns1240@naver.com>
2023-02-02 14:53:26 +0900

    Issues #281 fix: version upgrade
    - kotlin 1.7.20 -> 1.8.0 업그레이드
    - KOTLIN_COMPILER_EXTENSION 1.3.2 -> 1.4.0 으로 업그레이드
    - google-services 4.3.14 -> 4.3.15
    - JavaVersion 1.8 -> 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    id("com.android.application") version "7.3.1" apply false
-    id("com.android.library") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
 }
 
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.0")
-        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
     }
