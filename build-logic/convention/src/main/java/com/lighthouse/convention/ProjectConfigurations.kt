99e3ed538e4959f76a2acf10bdf1dc9e348f1a0d
김명석 <audxo112@naver.com>
2023-02-05 12:19:14 +0900

    Issues #285 chore: build-logic convention 추가

---
+++ build-logic/convention/src/main/java/com/lighthouse/convention/ProjectConfigurations.kt
@@ -0,0 +1,10 @@
+package com.lighthouse.convention
+
+import org.gradle.api.JavaVersion
+
+object ProjectConfigurations {
+    const val compileSdk = 33
+    const val minSdk = 21
+    const val targetSdk = 33
+    val javaVer = JavaVersion.VERSION_11
+}
