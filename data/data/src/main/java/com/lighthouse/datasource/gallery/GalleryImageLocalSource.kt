1ed75a58f742638c296cbadfa5db922331f92e95
김명석 <audxo112@naver.com>
2023-02-09 11:23:11 +0900

    Issues #287 feat: 필요없는 파일 삭제

--- data/data/src/main/java/com/lighthouse/datasource/gallery/GalleryImageLocalSource.kt
+++
@@ -1,7 +0,0 @@
-package com.lighthouse.datasource.gallery
-
-import com.lighthouse.beep.model.gallery.GalleryImage
-
-interface GalleryImageLocalSource {
-    suspend fun getImages(page: Int, limit: Int): List<GalleryImage>
-}
