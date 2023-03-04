0da6da25122bad90def417a992f69a6a2f2cbd73
mangbaam <pmb0836@gmail.com>
2023-03-04 21:46:41 +0900

    Issues #291 refactor: 위치 정보를 기존 xml → Fragment 에서 처리

--- presentation/src/main/java/com/lighthouse/presentation/ui/detailgifticon/StandardGifticonInfoFragment.kt
+++ presentation/src/main/java/com/lighthouse/presentation/ui/detailgifticon/StandardGifticonInfoFragment.kt
@@ -8,15 +8,21 @@ import com.lighthouse.presentation.R
 import com.lighthouse.presentation.databinding.FragmentStandardGifticonInfoBinding
 import com.lighthouse.presentation.ui.common.viewBindings
 import com.lighthouse.presentation.util.Geography
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class StandardGifticonInfoFragment : Fragment(R.layout.fragment_standard_gifticon_info) {
     private val binding: FragmentStandardGifticonInfoBinding by viewBindings()
     private val viewModel: GifticonDetailViewModel by activityViewModels()
 
+    @Inject
+    lateinit var geography: Geography
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
-        binding.geo = Geography(requireContext())
+        binding.geo = geography
         binding.lifecycleOwner = viewLifecycleOwner
     }
 }
